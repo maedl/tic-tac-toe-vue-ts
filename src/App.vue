03c22e3c8e51db18d7e0c1d8b614179336a25e08
Max Edlund <maxedlund88@gmail.com>
2023-05-26 14:04:47 +0200

    create StackIcons component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import StackIcons from './components/StackIcons.vue';
+
 </script>
 
 <template>
 
+<StackIcons />
  
 </template>
 
---
+++ src/components/StackIcons.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="../assets/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped></style>
