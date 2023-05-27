91df0fb14b1097af45ce845917f9d39251590e19
Max Edlund <maxedlund88@gmail.com>
2023-05-27 16:35:19 +0200

    add enum for playing pieces

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,7 +1,12 @@
+export enum playingPiece {
+  X = 'X',
+  O = 'O'
+}
+
 export class Player {
   constructor(
     public name: String,
-    public playingPiece: string,
+    public playingPiece: playingPiece,
     public score?: Number
   ) {
     this.score = 0;
