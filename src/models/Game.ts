48f12533f3fb63cafcf80cf6c5f69881b635d60d
Max Edlund <maxedlund88@gmail.com>
2023-05-30 21:18:38 +0200

    update and add method to Game class

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -2,9 +2,12 @@ import { BoardItem } from "./BoardItem"
 import { Player } from "./Player"
 
 export class Game {
- constructor(
-  public players: Player[],
-  public board: BoardItem[],
-  // public winner?: Player
- ) {}
+  constructor(
+    public players: Player[],
+    public board: BoardItem[],
+    public currentPlayerIndex: number
+  ) {}
+  nextTurn() {
+    this.currentPlayerIndex = (this.currentPlayerIndex === 0) ? 1 : 0;
+  }
 }
