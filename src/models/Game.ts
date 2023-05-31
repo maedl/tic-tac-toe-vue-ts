60aacd99a71b0886ea67e647612a331699ceed17
Max Edlund <maxedlund88@gmail.com>
2023-05-31 14:07:55 +0200

    remove logs and add enum

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -2,12 +2,18 @@ import { BoardItem } from "./BoardItem"
 import { GamePiece, Player } from "./Player"
 import { winningCombinations, GRID_LENGTH } from "./solutions";
 
+export enum gameEndType {
+  win = 'win',
+  draw = 'draw'
+}
+
 export class Game {
   constructor(
     public players: Player[],
     public board: BoardItem[],
     public currentPlayerIndex: number,
-    public turnCount: number
+    public turnCount: number,
+    public gameOver: boolean = false
   ) {}
   nextTurn() {
     this.currentPlayerIndex = (this.currentPlayerIndex === 0) ? 1 : 0;
@@ -21,15 +27,10 @@ export class Game {
 
       for (let j = 0; j < 3; j++) {
         threeInARow[j] = this.board[winningCombinations[i][j]].placedPiece
-        console.log(this.board[winningCombinations[i][j]])
       }
-      console.log(threeInARow);
       const firstPiece = threeInARow[0];
       if (firstPiece !== GamePiece.EMPTY && threeInARow.every(piece => piece === firstPiece)) {
-        return true;
-      }
-      else {
-        return false;
+        this.gameOver = true;
       }
     }
   }
