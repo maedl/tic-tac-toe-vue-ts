86b91c6409877e0b59e1c78b41dab83a23b1efb2
Max Edlund <maxedlund88@gmail.com>
2023-06-01 09:13:13 +0200

    add animations for modal and board

--- src/components/GridItem.vue
+++ src/components/GridItem.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue';
 import { BoardItem } from '../models/BoardItem';
 
 interface IGridItemProps {
@@ -11,6 +12,12 @@ const emit = defineEmits<{
 
 const props = defineProps<IGridItemProps>();
 
+let placedPiece = ref(props.boardItem.placedPiece);
+
+watchEffect(() => {
+  placedPiece.value = props.boardItem.placedPiece;
+});
+
 const handleClick = () => {
   emit('place-piece', props.boardItem.id)
 }
@@ -22,8 +29,31 @@ const handleClick = () => {
   md:h-36 hover:cursor-pointer"
   @click="handleClick"
   >
-    <div class="text-6xl md:text-7xl">
-      {{ props.boardItem.placedPiece }}
+  <Transition name="bounce">
+    <div class="text-6xl md:text-7xl" v-if="placedPiece">
+      {{ placedPiece }}
     </div>
+  </Transition>
   </div>
 </template>
+
+<style scoped>
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+</style>
--- src/components/ScoreModal.vue
+++ src/components/ScoreModal.vue
@@ -15,21 +15,35 @@ defineProps({
 </script>
 
 <template>
-<div
-    v-show="modalActive"
-    class="absolute w-full bg-emerald-700 h-screen top-0 left-0 flex justify-center px-8"
-    >
-    <div 
-      v-if="modalActive"
-      class="p-4 self-start mt-32 max-w-screen-md"
+<Transition>
+  <div
+      v-show="modalActive"
+      class="absolute w-full bg-emerald-700 h-screen top-0 left-0 flex justify-center px-8"
       >
-        <PlayerView v-for="player in currentGame?.players" :player="player"></PlayerView>
-      <button
-        @click="$emit('close-modal')"
-        class="btn my-2"
-      >
-        Close
-      </button>
+      <div
+        v-if="modalActive"
+        class="p-4 self-start mt-32 max-w-screen-md"
+        >
+          <PlayerView v-for="player in currentGame?.players" :player="player"></PlayerView>
+        <button
+          @click="$emit('close-modal')"
+          class="btn my-4"
+        >
+          Back
+        </button>
+      </div>
     </div>
-  </div>
+</Transition>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
